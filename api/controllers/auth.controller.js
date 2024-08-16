2cd9615a7e2d36d96f3ff65566246655487ff7e7
XYZ_of_properties <tushar@.com>
2024-08-17 00:06:39 +0530

    insomnia create

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+
+
+export const signup = (req , res, next)=>{
+    console.log(req.body);
+
+
+
+
+};
+    //const { username ,email , password} = req.body; 
+    //const hashedPassword = await bcryptjs.hashSync(password,10);
+    //const newUser = new User({ username,email,password: hashedPassword});
