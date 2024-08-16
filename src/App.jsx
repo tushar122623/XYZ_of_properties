0626fec701bebcfaf2bc85b465462925895ca0ec
XYZ_of_properties <tushar@.com>
2024-08-16 21:30:12 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/Signin';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
-export default function () {
-  return (
-    <div>
-      tushar
-    </div>
-  )
+export default function App() {
+  return <BrowserRouter>
+  <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/sign-in" element={<SignIn />}/>
+      <Route path="/sign-up" element={<SignUp />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/profile" element={<Profile />}/>
+  </Routes>
+  </BrowserRouter>
+  
 }
